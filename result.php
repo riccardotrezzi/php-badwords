d3459289255301a6dd1c55514121f00a503d1445
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-10-01 23:09:59 +0200

    aggiunto ultima parte esercizio

--- result.php
+++ result.php
@@ -2,6 +2,11 @@
     $phrase = 'Ahhh, cavolo mi è caduto il latte';
     var_dump(strlen($phrase));
 
+
+    $censoredPhrase = str_replace('cavolo', '***', $phrase);
+    var_dump($censoredPhrase);
+    var_dump(strlen($censoredPhrase));
+
 ?>
 
 <!DOCTYPE html>
@@ -15,8 +20,12 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
-        <p class="fs-3">
+        <p class="container fs-3">
             Ahhh, <?php echo $_POST['badWords'];?> mi è caduto il latte!
+        </p>
+
+        <p class="container fs-3">
+            <?php echo $censoredPhrase ?>
         </p>
     </body>
 </html>
